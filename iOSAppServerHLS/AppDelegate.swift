84fb9185905e6266652215feeebb4e5353eb77c6
aseo <aseo@yahoo-corp.jp>
2020-05-24 04:04:38 +0900

    first commit

--- iOSAppServerHLS/AppDelegate.swift
+++ iOSAppServerHLS/AppDelegate.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import GCDWebServers
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var webServer: GCDWebServer?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        webServer = GCDWebServer()
+        /**
+         * [GCDWebServer](https://github.com/swisspol/GCDWebServer)
+         * [HLSの用意 ~ MIMETypeの話の参考](https://www.yaz.co.jp/tec-blog/web%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9/212)
+         */
+        let m3u8Path = Bundle.main.path(forResource: "prog_index", ofType: "m3u8", inDirectory: "HLSDatas")!
+        for filePath in Bundle.main.paths(forResourcesOfType: "ts", inDirectory: "HLSDatas") {
+            // アクセスするファイル名が取得できるかを確認する
+            guard let fileName = filePath.components(separatedBy: "/").last else { continue }
+            webServer?.addGETHandler(
+                forPath: "/\(fileName)",
+                filePath: filePath,
+                isAttachment: true,
+                cacheAge: 3600,
+                allowRangeRequests: true
+            )
+            webServer?.addHandler(forMethod: "GET",
+                                  path: "/\(fileName)",
+                                  request: GCDWebServerRequest.self,
+                                  processBlock: { (request) -> GCDWebServerResponse? in
+                                    let tsData = try! Data(contentsOf: URL(fileURLWithPath: filePath))
+                                    return GCDWebServerDataResponse(data: tsData,
+                                                                    contentType: "video/mp2t")
+            })
+        }
+        // M3U8ファイルをWebサーバに追加する
+        webServer?.addHandler(forMethod: "GET",
+                              path: "/prog_index.m3u8",
+                              request: GCDWebServerRequest.self,
+                              processBlock: { (request) -> GCDWebServerResponse? in
+                                let m3u8Data = try! Data(contentsOf: URL(fileURLWithPath: m3u8Path))
+                                return GCDWebServerDataResponse(data: m3u8Data,
+                                                                contentType: "application/vnd.apple.mpegurl")
+        })
+        webServer?.start(withPort: 8080, bonjourName: "GCD Web Server")
         return true
     }
 
--- iOSAppServerHLS/ViewController.swift
+++ iOSAppServerHLS/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let avPlayerVC = AVPlayerViewController()
+        let avPlayer = AVPlayer(url: URL(string: "http://localhost:8080/prog_index.m3u8")!)
+        avPlayerVC.player = avPlayer
+        present(avPlayerVC, animated: true) { avPlayer.play() }
+    }
 
 }
 
