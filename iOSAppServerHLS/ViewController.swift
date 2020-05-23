1d7d991ce799d246c48abbedd0fc3b3bb0ab1ff0
aseo <aseo@yahoo-corp.jp>
2020-05-24 02:09:37 +0900

    Initial Commit

---
+++ iOSAppServerHLS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAppServerHLS
+//
+//  Created by aseo on 2020/05/24.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
